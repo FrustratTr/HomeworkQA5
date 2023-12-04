72d9a594959c50fa25e12c6c972218f779046072
Mark <mark.bogachev.00@mail.ru>
2023-12-04 12:20:14 +0300

    test changed

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -8,10 +8,7 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 import java.time.Duration;
 
-
 public class CardDeliveryTest {
-
-
 
     @Test
     public void shouldSubmitCardDeliveryForm() {
@@ -20,11 +17,11 @@ public class CardDeliveryTest {
 
         $("[data-test-id='city'] input").setValue("Москва");
         String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $("[data-test-id='date'] input").doubleClick().sendKeys("\b\b\b\b\b\b\b\b\b\b" + date);
+        $("[data-test-id='date'] input").doubleClick().sendKeys(date);
         $("[data-test-id='name'] input").setValue("Иван Иванов");
         $("[data-test-id='phone'] input").setValue("+79001234567");
         $("[data-test-id='agreement']").click();
-        $(By.cssSelector("button.button_view_extra.button_size_m.button_theme_alfa-on-white")).click();
+        $(By.cssSelector("button.button_view_extra")).click();
         $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
         $("[data-test-id='notification'] .notification__content")
                 .shouldHave(text("Встреча успешно забронирована на " + date));
